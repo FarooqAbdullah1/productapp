3b3eb27a99dca08a887c2ac35dc20dd53957ad77
Hafiz Tanzeel Shamshad <hafiztanzeel@gmail.com>
2024-10-04 09:33:37 +0500

    first commit

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../models/product_model.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  final Product product;
+
+  const ProductDetailScreen({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Product Details')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(product.imageUrl),
+            SizedBox(height: 16),
+            Text(
+              product.title,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              product.description,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '\$${product.price}',
+              style: TextStyle(fontSize: 20, color: Colors.green),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
